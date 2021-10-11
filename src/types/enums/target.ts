9d4f047d21084daa757422d5909f7c6977373ba8
RaenonX <raenonx0710@gmail.com>
2021-10-11 03:22:23 -0500

    Added simple hit target enum
    Signed-off-by: RaenonX <raenonx0710@gmail.com>

--- src/types/enums/target.ts
+++ src/types/enums/target.ts
@@ -1,8 +1,28 @@
 export enum Target {
   SELF = 1,
-  SELF_SURROUNDING = 2,
+  SELF_SKILL_FIELD = 2,
   ENEMY = 3,
-  FIELD = 4,
   TEAM = 6,
   TEAM_LOWEST_HP = 7,
+  HIT_OR_GUARDED_RECORD = 8,
+  HIT_DEPENDENT_SELF = 15,
 }
+
+export enum TargetSimple {
+  SELF,
+  SELF_SURROUNDING,
+  ENEMY,
+  FIELD,
+  TEAM,
+  TEAM_LOWEST_HP,
+}
+
+export const TargetToSimple: {[target in Target]: TargetSimple} = {
+  [Target.SELF]: TargetSimple.SELF,
+  [Target.SELF_SKILL_FIELD]: TargetSimple.SELF_SURROUNDING,
+  [Target.ENEMY]: TargetSimple.ENEMY,
+  [Target.TEAM]: TargetSimple.TEAM,
+  [Target.TEAM_LOWEST_HP]: TargetSimple.TEAM_LOWEST_HP,
+  [Target.HIT_OR_GUARDED_RECORD]: TargetSimple.ENEMY,
+  [Target.HIT_DEPENDENT_SELF]: TargetSimple.SELF,
+};
