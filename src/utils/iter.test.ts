0b822a0d1e19f0074280d67554cc794b99a47845
RaenonX <raenonx0710@gmail.com>
2021-10-11 16:36:16 -0500

    Added iter tools
    Signed-off-by: RaenonX <raenonx0710@gmail.com>

---
+++ src/utils/iter.test.ts
@@ -0,0 +1,40 @@
+import {combinations, product} from './iter';
+
+
+describe('Iter tools', () => {
+  it('returns correct combinations', async () => {
+    const comb = combinations([1, 2, 3]);
+
+    expect(comb).toStrictEqual([
+      [1],
+      [2],
+      [1, 2],
+      [3],
+      [1, 3],
+      [2, 3],
+      [1, 2, 3],
+    ]);
+  });
+
+  it('returns correct cartesian products depth 0', async () => {
+    const prod = product([1, 2], [4, 5]);
+
+    expect(prod).toStrictEqual([
+      [1, 4],
+      [1, 5],
+      [2, 4],
+      [2, 5],
+    ]);
+  });
+
+  it('returns correct cartesian products depth 1', async () => {
+    const prod = product([[1, 2], [3]], [[4], [5, 6]]);
+
+    expect(prod).toStrictEqual([
+      [1, 2, 4],
+      [1, 2, 5, 6],
+      [3, 4],
+      [3, 5, 6],
+    ]);
+  });
+});
---
+++ src/utils/iter.ts
@@ -0,0 +1,17 @@
+export const combinations = <T, >(array: Array<T>): Array<Array<T>> => {
+  return new Array(1 << array.length)
+    .fill(null)
+    .map((_, i) => array.filter((_, j) => i & 1 << j))
+    .filter((arr) => arr.length > 0);
+};
+
+export const product = <T, >(...arr: Array<Array<T>>): Array<Array<T>> => (
+  arr.reduce<Array<Array<T>>>(
+    (a, b) => (
+      a
+        .map((x) => b.map((y) => x.concat(y)))
+        .reduce((c, d) => c.concat(d), [])
+    ),
+    [[]],
+  )
+);
