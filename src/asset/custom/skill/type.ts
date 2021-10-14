b230f37027da741f20fe5d4c372ec36dd2066525
RaenonX <raenonx0710@gmail.com>
2021-10-14 01:16:59 -0500

    Added buff field boost params for attacking skill data
    Signed-off-by: RaenonX <raenonx0710@gmail.com>

--- src/asset/custom/skill/type.ts
+++ src/asset/custom/skill/type.ts
@@ -1,15 +1,24 @@
 import {ConditionComposite} from '../../../types/enums/condition';
 import {CustomAssetText} from '../../../types/resources/unit/text';
+import {DeepPartial} from '../../../utils/type';
 
+
+export type SkillDataParams = {
+  buff: {
+    inEffect: number,
+    field: {
+      self: number,
+      ally: number,
+    }
+  }
+};
 
 export type SkillData = {
   identifier: Array<CustomAssetText>,
   conditions: Array<ConditionComposite>,
   skillDataId: number,
   hitLabels: Array<string>,
-  params: {
-    buffBoostInEffect?: number
-  }
+  params: DeepPartial<SkillDataParams>
 };
 
 export type SkillDataCollection = Array<SkillData>;
