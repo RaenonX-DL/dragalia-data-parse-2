036f8a5f26c40745d1c0302dc53f76821413a3b9
RaenonX <raenonx0710@gmail.com>
2021-10-28 23:06:41 -0500

    Improved main script to accept commands
    Signed-off-by: RaenonX <raenonx0710@gmail.com>

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,23 @@ import {AssetProcessor} from './process/processor';
 import {loadYaml} from './utils/load';
 
 
-const environment = new LocalEnvironment(loadYaml(process.argv[2]));
+const command = process.argv[2];
+const environment = new LocalEnvironment(loadYaml(process.argv[3] || 'config.yaml'));
 const processor = new AssetProcessor(environment);
 
-processor.export();
+const main = async () => {
+  if (command === 'export') {
+    processor.export();
+  } else if (command === 'build') {
+    await processor.build();
+  } else {
+    console.log('Available commands: `export` and `build`.');
+  }
+};
+
+(async () => {
+  await main();
+})().catch((e) => {
+  console.error(e);
+  process.exit(1);
+});
